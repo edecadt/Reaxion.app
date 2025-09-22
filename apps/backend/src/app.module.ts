e2dd50b91252012384b2c65536afd277a673c6b9
Simon Delcuze <simon.delcuze@epitech.eu>
2025-09-22 11:51:40 +0200

    fix(backend): loading port from dotenv

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { join, resolve } from 'node:path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        join(__dirname, '..', '.env.local'),
+        join(__dirname, '..', '.env.development'),
+        join(__dirname, '..', '.env'),
+        resolve(process.cwd(), 'apps', 'backend', '.env.local'),
+        resolve(process.cwd(), 'apps', 'backend', '.env.development'),
+        resolve(process.cwd(), 'apps', 'backend', '.env'),
+        resolve(process.cwd(), '.env.local'),
+        resolve(process.cwd(), '.env.development'),
+        resolve(process.cwd(), '.env'),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
