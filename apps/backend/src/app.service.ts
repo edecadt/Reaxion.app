5c736db7e2f017641a65286450a39c0d92c5cd01
Ezio Decadt <eziodecadt04@gmail.com>
2025-09-29 18:32:39 +0200

    feat(api): update /about.json according to subject

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -13,13 +13,25 @@ export class AppService {
   buildAboutPayload(clientHost: string | undefined) {
     const services = this.serviceRegistry.getPublicServices();
 
+    const formattedServices = services.map((service) => ({
+      name: service.name.toLowerCase(),
+      actions: service.actions.map((action) => ({
+        name: action.name,
+        description: action.description,
+      })),
+      reactions: service.reactions.map((reaction) => ({
+        name: reaction.name,
+        description: reaction.description,
+      })),
+    }));
+
     return {
       client: {
         host: clientHost ?? '',
       },
       server: {
         current_time: Math.floor(Date.now() / 1000),
-        services,
+        services: formattedServices,
       },
     };
   }
