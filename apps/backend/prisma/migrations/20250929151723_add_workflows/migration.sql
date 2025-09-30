1c106745548b4b8af28f7346801383142618aab2
Charles Ignoux <146704294+charliinew@users.noreply.github.com>
2025-09-30 10:28:16 +0200

    feat: Workflow and WorkflowNode tables with constraints

--- apps/backend/prisma/migrations/20250929151723_add_workflows/migration.sql
+++ apps/backend/prisma/migrations/20250929151723_add_workflows/migration.sql
@@ -1,8 +1,6 @@
--- AlterTable
 ALTER TABLE "public"."User" ALTER COLUMN "passwordHash" DROP DEFAULT,
 ALTER COLUMN "updatedAt" DROP DEFAULT;
 
--- CreateTable
 CREATE TABLE "public"."Workflow" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -14,7 +12,6 @@ CREATE TABLE "public"."Workflow" (
     CONSTRAINT "Workflow_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
 CREATE TABLE "public"."WorkflowNode" (
     "id" TEXT NOT NULL,
     "workflowId" TEXT NOT NULL,
@@ -30,17 +27,12 @@ CREATE TABLE "public"."WorkflowNode" (
     CONSTRAINT "WorkflowNode_pkey" PRIMARY KEY ("id")
 );
 
--- CreateIndex
 CREATE INDEX "Workflow_userId_idx" ON "public"."Workflow"("userId");
 
--- CreateIndex
 CREATE INDEX "WorkflowNode_workflowId_idx" ON "public"."WorkflowNode"("workflowId");
 
--- CreateIndex
 CREATE INDEX "WorkflowNode_workflowId_position_idx" ON "public"."WorkflowNode"("workflowId", "position");
 
--- AddForeignKey
 ALTER TABLE "public"."Workflow" ADD CONSTRAINT "Workflow_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
--- AddForeignKey
 ALTER TABLE "public"."WorkflowNode" ADD CONSTRAINT "WorkflowNode_workflowId_fkey" FOREIGN KEY ("workflowId") REFERENCES "public"."Workflow"("id") ON DELETE CASCADE ON UPDATE CASCADE;
